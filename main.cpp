5b9e17a3cf467dbfdec74003d007ab53c35a4581
Jacek Maciupa <spaghetti147@gmail.com>
2019-01-09 21:35:37 +0100

    merge all project to one file :) handle input and connection protocols

--- main.cpp
+++ main.cpp
@@ -16,6 +16,10 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
+#include <sstream>
+
+//#include "clientPos.cpp"
+//#include "Positions.h"
 
 using namespace std;
 
@@ -23,6 +27,7 @@ class Client;
 
 int servFd;
 int epollFd;
+//Positions* positions = new Positions();
 
 std::unordered_set<Client*> clients;
 
@@ -32,8 +37,15 @@ uint16_t readPort(char * txt);
 
 void setReuseAddr(int sock);
 
+void sendPositionsToAll();
+
+void fdToStr(int fd, stringstream& ss);
+
+void intToStr(int num, stringstream& ss);
 
 void sendToAll(char * buffer);
+
+void sendToAll(stringstream& ss);
 
 void sendToAllBut(int fd, char * buffer);
 
@@ -42,16 +54,30 @@ struct Handler {
     virtual void handleEvent(uint32_t events) = 0;
 };
 
+struct clientPos{
+    int positionX;
+    int positionY;
+};
+
 class Client : public Handler{
     int _fd;
     int _points;
-    int _position[2];
+    clientPos _position;
+private:
+    void updateClientPos(char * message){
+        for(int i=0; i<64; i++){
+            cout<<message[i] << " ";
+        }
+        cout<<endl;
+    }
 
 public:
     Client(int fd) : _fd(fd) {
         epoll_event ee {EPOLLIN|EPOLLRDHUP, {.ptr=this}};
         epoll_ctl(epollFd, EPOLL_CTL_ADD, _fd, &ee);
         _points = 0;
+        _position.positionX = rand() % 640;
+        _position.positionY = rand() % 480;
     }
 
     virtual ~Client(){
@@ -63,8 +89,14 @@ public:
     // getters
     int fd() const {return _fd;}
     int points() const {return _points;}
+    const clientPos &get_position() const {
+        return _position;
+    }
 
     // setters
+    void set_position(const clientPos &_position) {
+        Client::_position = _position;
+    }
     void points(int a) {_points = a;}
 
     virtual void handleEvent(uint32_t events) override {
@@ -82,22 +114,26 @@ public:
         ssize_t count  = ::read(_fd, dataFromRead, sizeof(dataFromRead)-1);
         if(count <= 0) events |= EPOLLERR;
 
-//        for (int i=0; i<8; i++) data[i] = dataFromRead[i];
+        this->updateClientPos(dataFromRead);
 
-        sendToAll(dataFromRead);
-
-        printf("client: %d \t READ: %s \n", _fd, dataFromRead);
+        sendPositionsToAll();
+//        printf("client: %d \t READ: %s \n", _fd, dataFromRead);
     }
 
     void write(char * buffer){
         if ( ::write(_fd, buffer, strlen(buffer)) != (int) strlen(buffer) ) perror("write failed");
-        printf("client: %d \t WRITE: %s \n", _fd, buffer);
+//        printf("client: %d \t WRITE: %s \n", _fd, buffer);
+    }
+
+    void write(stringstream& ss){
+        if ( ::write(_fd, ss.str().c_str(), strlen(ss.str().c_str())) != (int) strlen(ss.str().c_str()) ) perror("write failed");
+//        printf("client: %d \t WRITE: %s \n", _fd, ss.str().c_str());
     }
 
     void remove() {
         clients.erase(this);
         delete this;
-        printf("client REMOVED \n");
+        sendPositionsToAll();
     }
 };
 
@@ -123,6 +159,7 @@ public:
                 sprintf(tmp, "%d", clientFd);
                 ::write(clientFd, tmp, strlen(tmp));
                 clients.insert(new Client(clientFd));
+                sendPositionsToAll();
             }
         }
 
@@ -200,6 +237,57 @@ void ctrl_c(int){
 
 
 // *************** SEND TO CLIENTS ******************
+
+
+void sendPositionsToAll(){
+    stringstream ss;
+    stringstream message;
+
+    message<<time(NULL);
+
+    int i = 0;
+    auto it = clients.begin();
+    while(it!=clients.end()){
+        Client * client = *it;
+
+        it++;
+        ss.str("");
+
+        fdToStr(client->fd(), ss);
+        fdToStr(client->points(), ss);
+        intToStr(client->get_position().positionX, ss);
+        intToStr(client->get_position().positionY, ss);
+
+        message<<ss.str().c_str();
+        i++;
+    }
+
+    for(i; i<4; i++){
+        message<<"0000000000";
+    }
+
+    sendToAll(message);
+}
+
+void fdToStr(int fd, stringstream& ss){
+    if(fd < 10)ss<<0<<fd;
+    else ss<<fd;
+}
+
+void intToStr(int num, stringstream& ss){
+    if(num < 100) ss<<0;
+    if(num < 10)ss<<0;
+    ss<<num;
+}
+
+void sendToAll(stringstream& ss){
+    auto it = clients.begin();
+    while(it!=clients.end()){
+        Client * client = *it;
+        it++;
+        client->write(ss);
+    }
+}
 
 void sendToAll(char * buffer){
     auto it = clients.begin();
